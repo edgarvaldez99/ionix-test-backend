ff63eb89ef56e64ff0bd543c3cf655efd225649a
Edgar Valdez <edgarvaldez99@gmail.com>
2022-11-11 16:49:12 -0300

    added ValidationPipe configuration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,6 +13,18 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      validationError: {
+        target: false,
+      },
+      whitelist: true,
+    }),
+  );
   await app.listen(port);
 }
 bootstrap();
