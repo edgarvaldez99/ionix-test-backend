5427935608d70ab8dec8d48793b78e9a12fb2a6c
Edgar Valdez <edgarvaldez99@gmail.com>
2022-11-10 22:31:02 -0300

    added Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 3100;
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('IONIX')
+    .setDescription('The Ionix API application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(port);
 }
 bootstrap();
